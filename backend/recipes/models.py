a5f2b7009f263a1effd05e95c70b8b427085f24c
Michael M <trdMichaelM@yandex.ru>
2022-01-20 18:17:51 +0300

    created all models

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -4,26 +4,6 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class Recipe(models.Model):
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='recipes'
-    )
-    name = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='images/')
-    text = models.TextField()
-    # ingredients
-    # tags - one to many
-    cooking_time = models.PositiveIntegerField(default=1)
-
-    # is_favorited
-    # is_in_shopping_cart
-
-    def __str__(self):
-        return self.name
-
-
 class Tag(models.Model):
     name = models.CharField(max_length=200, unique=True)
     color = models.CharField(max_length=7, null=True, unique=True)
@@ -32,29 +12,77 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        ordering = ['pk']
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=200)
     measurement_unit = models.CharField(max_length=200)
 
+    class Meta:
+        ordering = ['pk']
+
 
 class IngredientInRecipe(models.Model):
-    pass
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=1)
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes'
+    )
+    pub_date = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    text = models.TextField()
+    ingredients = models.ManyToManyField(
+        IngredientInRecipe,
+        through='IngredientInRecipeForRecipe'
+    )
+    tags = models.ManyToManyField(Tag, through='TagForRecipe')
+    cooking_time = models.PositiveIntegerField(default=1)
+
+    # is_favorited
+    # is_in_shopping_cart
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-pub_date']
+
+
+class IngredientInRecipeForRecipe(models.Model):
+    ingredient = models.ForeignKey(IngredientInRecipe,
+                                   on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+
+class TagForRecipe(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
 
 
 class Favorite(models.Model):
-    pass
-    # user
-    # id recipe
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='favorites')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE,
+                               related_name='favorites')
 
 
 class Cart(models.Model):
-    pass
-    # user
-    # id recipe
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='purchases')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE,
+                               related_name='buyers')
 
 
 class Subscription(models.Model):
-    pass
-    # id user
-    # id owner
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='subscriptions')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='subscribers')
