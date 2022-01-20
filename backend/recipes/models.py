181bc08e15e9b91c8a2463d67b9f6a5eeef8358e
Michael M <trdMichaelM@yandex.ru>
2022-01-20 18:20:41 +0300

    [upd] model Ingredient

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -20,6 +20,9 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=200)
     measurement_unit = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         ordering = ['pk']
 
