9afb42c7dd46e8e906d3d59e8d1c219196a3e8d0
Michael M <trdMichaelM@yandex.ru>
2022-02-05 17:56:45 +0300

    setup debug False

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', default='SUP3R-S3CR3T-K3Y-F0R-MY-PR0J3CT')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
